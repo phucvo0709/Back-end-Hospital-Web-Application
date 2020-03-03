a3147a0384c2985ed3c742d69378edfecda44670
Phuc Vo <phucvo0709@gmail.com>
2020-03-03 11:52:01 +0700

    fix css

--- routes/api/customers.js
+++ routes/api/customers.js
@@ -3,37 +3,19 @@ const router = express.Router();
 const controllerCustomer = require("../../controllers/customer");
 const middlewareCustomer = require("../../middleware/customer");
 
-// @route  GET api/customers
-// @desc   Get All Customers
-// @access Public
-router.get("/", controllerCustomer.getCustomers);
+// @route api/customers
+router
+  .get("/", controllerCustomer.getCustomers)
+  .post(
+    "/",
+    [middlewareCustomer.postCustomer],
+    controllerCustomer.postCustomer
+  );
 
-// @route  POST api/customers/:id
-// @desc   Get Customer By id
-// @access Public
-router.get("/:id", controllerCustomer.getCustomer);
-
-// @route  POST api/customers/
-// @desc   Create Customers
-// @access Public
-router.post(
-  "/",
-  [middlewareCustomer.postCustomer],
-  controllerCustomer.postCustomer
-);
-
-// @route    PUT api/customers/:id
-// @desc     Put Customer By id
-// @access   Public
-router.put(
-  "/:id",
-  [[middlewareCustomer.putCustomer]],
-  controllerCustomer.putCustomer
-);
-
-// @route    DELETE api/customers/:id
-// @desc     Delete a customer
-// @access   Private
-router.delete("/:id", controllerCustomer.deleteCustomer);
+// @route api/customers/:id
+router
+  .get("/:id", controllerCustomer.getCustomer)
+  .put("/:id", [middlewareCustomer.putCustomer], controllerCustomer.putCustomer)
+  .delete("/:id", controllerCustomer.deleteCustomer);
 
 module.exports = router;
