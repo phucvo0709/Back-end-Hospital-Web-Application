4dc3e0475632f03764352be9b9947dffce61bafd
Phuc Vo <phucvo0709@gmail.com>
2020-03-06 11:15:23 +0700

    add emit socket

--- socket/customers.js
+++ socket/customers.js
@@ -1,7 +1,8 @@
 exports = module.exports = function(io) {
   io.sockets.on("connection", function(socket) {
     socket.on("newCustomers", function() {
-      console.log("newCustomers");
+      console.log("new customers");
+      io.emit("haveNewCustomers");
     });
   });
 };
