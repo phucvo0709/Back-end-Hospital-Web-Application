858a4ac6e47e5c5c90fcb7b2c7e9f8317b62238d
Phuc Vo <phucvo0709@gmail.com>
2020-03-06 15:59:00 +0700

    add broadcast

--- socket/index.js
+++ socket/index.js
@@ -2,10 +2,10 @@ exports = module.exports = function(io) {
   io.sockets.on("connection", function(socket) {
     console.log(`a device ${socket.id} connected`);
     socket.on("newRooms", function() {
-      io.emit("haveNewRooms", socket.id);
+      socket.broadcast.emit("haveNewRooms", socket.id);
     });
     socket.on("newCustomers", function() {
-      io.emit("haveNewCustomers", socket.id);
+      socket.broadcast.emit("haveNewCustomers", socket.id);
     });
     socket.on("disconnect", function() {
       console.log(`device ${socket.id} disconnected`);
